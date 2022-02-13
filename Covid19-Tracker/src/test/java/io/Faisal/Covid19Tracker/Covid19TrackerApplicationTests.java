7268f0e73d228ecea0eaaade7b52189bebca489b
Igal <igafaisa@amazon.com>
2022-02-13 12:24:22 -0700

    Initial commit

---
+++ Covid19-Tracker/src/test/java/io/Faisal/Covid19Tracker/Covid19TrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.Faisal.Covid19Tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Covid19TrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
